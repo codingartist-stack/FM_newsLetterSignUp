6fd202acbd2875ec505f868658047f9728a3ca8e
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-07 14:15:29 -0700

    grabbed the thanks pop up and user email span

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const subBtn = document.getElementById('subButton');
 const emailError = document.getElementById('errorMessage');
 const dismissBtn = document.getElementById('dismissBtn');
 
+const thanksPopUp = document.getElementsByClassName('thanksPopUp');
+const userEmail = document.getElementById('userEmail');
+
 subBtn.addEventListener();
