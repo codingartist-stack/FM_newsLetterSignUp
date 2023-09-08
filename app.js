c0a4848dd02bd9b219d2ea514d38444858ec0ded
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-08 10:41:35 -0700

    got the js to remove the subscription elements and add the thanks pop up. when you click dismiss it reloads the page.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const subscriptionContainer = document.getElementById('subscriptionContainer');
+
 const form = document.getElementById('form');
 const emailInput = document.getElementById('email');
 const subBtn = document.getElementById('subButton');
 const emailError = document.getElementById('errorMessage');
 const dismissBtn = document.getElementById('dismissBtn');
 
-const thanksPopUp = document.getElementsByClassName('thanksPopUp');
+const thanksPopUp = document.getElementById('thanksPopUp');
 const userEmail = document.getElementById('userEmail');
 
 const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -17,14 +19,22 @@ form.addEventListener('submit', (e) => {
   if (emailInput.value.match(mailFormat)) {
     e.preventDefault();
     userEmail.innerText = emailInput.value;
+    thanksPopUp.classList.remove('hidden');
+    subscriptionContainer.classList.add('hidden');
   } else {
     messages.push('Valid email required');
   }
   if (messages.length > 0) {
     e.preventDefault();
-    emailError.innerText = messages.join(', ');
+    emailError.innerText = messages[0];
     emailInput.classList.add('invalid');
   }
 
   console.log(messages);
 });
+
+const refresh = () => {
+  location.reload();
+};
+
+dismissBtn.addEventListener('click', refresh);
