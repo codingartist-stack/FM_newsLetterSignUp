b302743585f1bdc15b4eb1b82ecbb580b408011a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-07 18:47:40 -0700

    got the email validation working. need to do some css and get the dismiss button to refresh the page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const form = document.getElementById('form');
 const emailInput = document.getElementById('email');
 const subBtn = document.getElementById('subButton');
 const emailError = document.getElementById('errorMessage');
@@ -6,4 +7,24 @@ const dismissBtn = document.getElementById('dismissBtn');
 const thanksPopUp = document.getElementsByClassName('thanksPopUp');
 const userEmail = document.getElementById('userEmail');
 
-subBtn.addEventListener();
+const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+form.addEventListener('submit', (e) => {
+  let messages = [];
+  if (emailInput.value === '' || emailInput.value === null) {
+    messages.push('Valid email required');
+  }
+  if (emailInput.value.match(mailFormat)) {
+    e.preventDefault();
+    userEmail.innerText = emailInput.value;
+  } else {
+    messages.push('Valid email required');
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    emailError.innerText = messages.join(', ');
+    emailInput.classList.add('invalid');
+  }
+
+  console.log(messages);
+});
